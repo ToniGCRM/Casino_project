8e77a86195880b359bc8e5d3f1186950077016cc
Toni Galata <batscott@gmail.com>
2020-02-20 20:47:54 -0700

    made menu

--- casino.rb
+++ casino.rb
@@ -1,3 +1,10 @@
+require_relative 'blackjack.rb'
+require_relative 'cards.rb'
+require_relative 'dice.rb'
+require_relative 'hilo.rb'
+require_relative 'roulette.rb'
+require_relative 'slots.rb'
+
 # Start game player has a name and an initial bankroll
 # Player can go to different games via menu
 # Slots
@@ -7,27 +14,47 @@
 
 # get name
 # get bankroll
-class Casino
-  
+def welcome
 puts "Welcome to the Casino"
-puts "please enter your name"
-@player = gets.strip 
-puts "please enter your bankroll $"
-@bankroll = gets.to_f
-gets
-
+    puts "please enter your name"
+    @player = gets.strip 
+    puts "please enter your bankroll $"
+    @bankroll = gets.to_f
+    Casino.new(@player, @bankroll)
 end
 
+class Casino
+  attr_accessor :player, :bankroll
+  def initialize (player, bankroll)
+    @player = player
+    @bankroll = bankroll
+    game_choice
+  end
 
-#put their money in an account that deducts or grows as they lose or win
+  def game_choice
+    puts "What game would you like to play?"
+    puts "
+    1) Blackjack
+    2) Hi-Lo
+    3) Roulette
+    4) Slots"
+    player_choice = gets.to_i
+    case
+    when player_choice == 1
+      blackjack
+    when player_choice == 2
+      hilo
+    when player_choice == 3
+      roulette
+    when player_choice == 4
+      slots
+    else 
+      "Invalid selection, please enter a number 1-4"
+      game_choice
+    end
+  end
+end
 
-# blackjack - Nick
+#choose your game
+welcome
 
-#hi low - Max
-
-# roulette - sam
-
-# slots - toni
-
-#cash out with bankroll
-# if negative, threaten to break kneecaps
