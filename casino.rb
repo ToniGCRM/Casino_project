6f3b58887dc1842acbdb3761b105b997c68f2857
Toni Galata <batscott@gmail.com>
2020-02-20 20:23:31 -0700

    push to github

---
+++ casino.rb
@@ -0,0 +1,33 @@
+# Start game player has a name and an initial bankroll
+# Player can go to different games via menu
+# Slots
+# High / Low
+# Player places bet and wins / loses (hint: rand)
+# Player's bankroll goes up and down with wins and losses
+
+# get name
+# get bankroll
+class Casino
+  
+puts "Welcome to the Casino"
+puts "please enter your name"
+@player = gets.strip 
+puts "please enter your bankroll $"
+@bankroll = gets.to_f
+gets
+
+end
+
+
+#put their money in an account that deducts or grows as they lose or win
+
+# blackjack - Nick
+
+#hi low - Max
+
+# roulette - sam
+
+# slots - toni
+
+#cash out with bankroll
+# if negative, threaten to break kneecaps
