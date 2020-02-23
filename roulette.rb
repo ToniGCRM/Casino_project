8a21ae9b21a6cb313e694447a8dba69a7b006290
swjulkunen <61258082+swjulkunen@users.noreply.github.com>
2020-02-23 12:47:17 -0700

    Add files via upload

---
+++ roulette.rb
@@ -0,0 +1,27 @@
+def greeting 
+`clear`
+puts "\n\n\n"
+  puts "🎰🎰🎰🎰🎲🎲🎲💰💰💰💰 Welcome To Sam's Roulette Table 💰💰💰🎲🎲🎲🎰🎰🎰🎰"
+puts "\n\n\n\n"
+end
+
+def roulette_menu 
+  puts greeting 
+  
+  puts "     We Roll The Dice! You choose your slice. 
+
+  1. Bet Red
+  2. Bet Black 
+  3. Choose a number to bet on
+  4. Go back to the casion 
+  5. Rules  
+  
+  "
+  puts "Choose an option above"
+  user_input = gets.chomp
+  case user_iput 
+  when 1 
+  end 
+
+end 
+puts roulette_menu
